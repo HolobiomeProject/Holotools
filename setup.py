ed4962df60354fc50edd58f255d92ca2731b0f02
HolobiomeProject <barry@holobiome.org>
2020-09-24 17:19:11 -0400

    v0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md","r") as fh:
 
 setup(
 name='Holotools', #pypi name
-version = '0.0.4',
+version = '0.0.5',
 description='Python Code Base Used by the Holobiome Team',
 py_modules=['Holotools',
             'ht_clustering',
