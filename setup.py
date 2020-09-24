26e49b70d2b09546f7e586904d6b19b0b9105d58
HolobiomeProject <barry@holobiome.org>
2020-09-24 17:37:44 -0400

    0.0.8

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ setup(
 name='Holotools', #pypi name
 version = '0.0.7',
 description='Python Code Base Used by the Holobiome Team',
-py_modules=['Holotools',
-            'ht_clustering',
-            'ht_trim'], #'import' name
+py_modules=['Holotools','holotools'], #'import' name
 package_dir={'':'src'},
 long_description=long_description,
 long_description_content_type="text/markdown",
