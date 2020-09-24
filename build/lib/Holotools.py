cdbbd00408dd09b83af12f22a51da1eb3ed9b712
HolobiomeProject <barry@holobiome.org>
2020-09-24 17:42:26 -0400

    0.0.8

--- build/lib/Holotools.py
+++ build/lib/Holotools.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''Binders and Tools for Clustering of Bacterial Sequences'''
-from ht_biop import *
-from ht_trim import *
-
-
-d = fdict('test_16.fna')
-td = {}
-for k,v in d.items():
-    print(k)
-    td[k]=sliding_window(v)
-print(td)
+# from hbiop import *
+# from htrim import *
+#
+#
+# d = fdict('test_16.fna')
+# td = {}
+# for k,v in d.items():
+#     print(k)
+#     td[k]=sliding_window(v)
+# print(td)
