b43b2badb921442517e64ed76bc64ba181efa760
HolobiomeProject <barry@holobiome.org>
2020-09-24 12:46:56 -0400

    pypi trim

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md","r") as fh:
 
 setup(
 name='Holotools', #pypi name
-version = '0.0.3',
+version = '0.0.4',
 description='Python Code Base Used by the Holobiome Team',
 py_modules=['Holotools',
             'clustering',
