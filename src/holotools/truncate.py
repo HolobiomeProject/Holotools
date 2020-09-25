1081d61193137bbbf4dcc2b961462da41da04225
HolobiomeProject <barry@holobiome.org>
2020-09-25 11:30:00 -0400

    no delete of tmp

--- src/holotools/truncate.py
+++ src/holotools/truncate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Truncate Sequences to primer locations'''
-def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primers_2020.02.fna', primerblastdb = '/home/boom/amp/primers/unambiguous_primers_2020.02', startprimers = ['515F_mod'], endprimers = ['806R_Mod','806F_Mod'], min_pct = 90, plusleft = 0, plusright = 0, min_len_pct = 90,sides = 'both'):
+def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primers_2020.02.fna', primerblastdb = '/home/boom/amp/primers/unambiguous_primers_2020.02', startprimers = ['515F_mod'], endprimers = ['806R_Mod','806F_Mod'], min_pct = 90, plusleft = 0, plusright = 0, min_len_pct = 90,sides = 'both',del=True):
     from holotools import biop
     import os
     import shutil
@@ -20,6 +20,7 @@ def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primer
     fdf = pd.DataFrame()
     na = open('nopfound.tsv','w')
     na.write('query\tissue\tdetails\n')
+    trunc = {}
     # make tmp files of each seq blast and parse
     for k,v in d.items():
         out = open('%s/tmp/%s.fna'%(cwd,k),'w')
@@ -34,11 +35,12 @@ def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primer
             s = df[df.sseqid.isin(endprimers)]
             for i in endprimers:
                 plen = len(p[i])
-                print(plen*(min_len_pct/100))
                 ep = s.loc[s.sseqid==i]
                 ep = ep.loc[ep.length>=plen*(min_len_pct/100)]
                 if len(ep)<1:
                     na.write(k+'\tprimer not found\t%s\n'%i)
+                else:
+                    mmin = min(ep.qstart)
                 fdf = pd.concat([fdf,ep])
             # start primers
             s = df[df.sseqid.isin(startprimers)]
@@ -48,11 +50,21 @@ def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primer
                 ep = ep.loc[ep.length>=plen*(min_len_pct/100)]
                 if len(ep)<1:
                     na.write(k+'\tprimer not found\t%s\n'%i)
+                if side = 'right':
+                    mmax = max(ep.qend)
                 fdf = pd.concat([fdf,ep])
         except:
             na.write(k+'\tprimer not found\tall\n')
-    # shutil.rmtree('tmp')
+    if del == True:
+        shutil.rmtree('tmp')
+    if side == 'right':
+        trunc[k]=v[:mmax]
+    elif side == 'left':
+        trunc[k]=v[mmin:]
+    if side == 'both':
+        trunc[k]=v[mmin:mmax]
+
     na.close()
-    return fdf
+    return fdf, trunc
 
 # truncate2primer('test16.fna')
