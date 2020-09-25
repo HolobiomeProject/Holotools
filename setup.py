4c6fb3d2dcfbce6d79ba1a8e14ca91f6aa2b861c
HolobiomeProject <barry@holobiome.org>
2020-09-25 16:30:04 -0400

    truncation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md","r") as fh:
 
 setup(
 name='Holotools', #pypi name
-version = '0.0.9',
+version = '0.0.10',
 description='Python Code Base Used by the Holobiome Team',
 py_modules=['Holotools',
             'holotools',
