543e2a60b1617705eb3763f121f54f0627535197
HolobiomeProject <barry@holobiome.org>
2020-09-25 11:32:40 -0400

    no delete of tmp

--- src/holotools/truncate.py
+++ src/holotools/truncate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Truncate Sequences to primer locations'''
-def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primers_2020.02.fna', primerblastdb = '/home/boom/amp/primers/unambiguous_primers_2020.02', startprimers = ['515F_mod'], endprimers = ['806R_Mod','806F_Mod'], min_pct = 90, plusleft = 0, plusright = 0, min_len_pct = 90, sides = 'both', del = True):
+def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primers_2020.02.fna', primerblastdb = '/home/boom/amp/primers/unambiguous_primers_2020.02', startprimers = ['515F_mod'], endprimers = ['806R_Mod','806F_Mod'], min_pct = 90, plusleft = 0, plusright = 0, min_len_pct = 90, sides = 'both', deltmp = True):
     from holotools import biop
     import os
     import shutil
@@ -55,7 +55,7 @@ def truncate2primer(file, primerlist= '/home/boom/amp/primers/unambiguous_primer
                 fdf = pd.concat([fdf,ep])
         except:
             na.write(k+'\tprimer not found\tall\n')
-    if del == True:
+    if deltmp == True:
         shutil.rmtree('tmp')
     if side == 'right':
         trunc[k]=v[:mmax]
